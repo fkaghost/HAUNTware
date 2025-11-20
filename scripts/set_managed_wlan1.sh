fe4f06a60230f93fc48d6231df8cb6e0dc45cec1
fkaghost <119707955+fkaghost@users.noreply.github.com>
2025-11-19 16:05:30 -0800

    Update set_managed_wlan1.sh

--- scripts/set_managed_wlan1.sh
+++ scripts/set_managed_wlan1.sh
@@ -1,13 +1,9 @@
-#!/usr/bin/env bash
-set -e
-
+#!/bin/bash
 IFACE="wlan1"
 
-echo "[*] Returning wlan1 to managed mode"
-sudo ifconfig $IFACE down || true
-sudo iwconfig $IFACE mode managed
-sudo ifconfig $IFACE up
+sudo ip link set $IFACE down
+sudo iw dev $IFACE set type managed
+sudo ip link set $IFACE up
 
-sudo nmcli dev set $IFACE managed yes || true
-
-echo "[*] wlan1 is now in MANAGED MODE"
+iwconfig $IFACE
+echo "[INFO] wlan1 is now in MANAGED MODE"
