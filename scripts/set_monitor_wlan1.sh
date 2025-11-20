5348c653e6a3ec94997f80403554ee0b6a67eddc
fkaghost <119707955+fkaghost@users.noreply.github.com>
2025-11-19 16:45:08 -0800

    Simplify menu and remove bettercap tooling

--- scripts/set_monitor_wlan1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-IFACE="wlan1"
-
-sudo ip link set $IFACE down
-sudo iw dev $IFACE set type monitor
-sudo ip link set $IFACE up
-
-iwconfig $IFACE
-echo "[INFO] wlan1 is now in MONITOR MODE"
